7b1342d06464738659c089b557f0f39fcbc11621
sebuszqo <sekn.kn@gmail.com>
2022-09-08 17:37:08 +0200

    added some validation

--- Projects/cookie-maker-project/routes/configurator.js
+++ Projects/cookie-maker-project/routes/configurator.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {COOKIE_ADDONS, COOKIE_BASES} = require("../data/cookies.-data");
 
 
 const configuratorRouter = express.Router();
@@ -7,11 +8,22 @@ const configuratorRouter = express.Router();
 configuratorRouter
     .get('/select-base/:baseName', (req, res) => {
         const {baseName} = req.params;
+        if(!COOKIE_BASES[baseName]){
+            return res.render('error', {
+                description: `There is no such base as ${baseName}.`
+            })
+        }
         res.cookie('cookieBase', baseName);
         res.render('configurator/base-selected', {baseName});
     })
     .get('/select-addons/:addonName', (req, res) => {
         const {addonName} = req.params;
+
+        if(!COOKIE_ADDONS[addonName]){
+            return res.render('error', {
+                description: `There is no such addon as ${addonName}.`
+            })
+        }
         const {cookieAddons} = req.cookies;
         if (!cookieAddons) {
             const listOfAddons = [];
@@ -21,7 +33,10 @@ configuratorRouter
         } else {
             const cookieAddonsAfterParse = JSON.parse(cookieAddons);
             if (cookieAddonsAfterParse.includes(addonName)) {
-                res.cookie('cookieAddons', JSON.stringify(cookieAddonsAfterParse));
+                // res.cookie('cookieAddons', JSON.stringify(cookieAddonsAfterParse));
+                return res.render('error', {
+                    description: `${addonName} has been already added. You cannot added things twice!.`
+                })
             } else {
                 cookieAddonsAfterParse.push(addonName)
                 res.cookie('cookieAddons', JSON.stringify(cookieAddonsAfterParse));
