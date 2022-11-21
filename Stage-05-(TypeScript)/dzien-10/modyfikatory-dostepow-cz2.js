9d5075919c9a2371c0b8482d5354591cc1970852
sebuszqo <sekn.kn@gmail.com>
2022-11-21 21:02:42 +0100

    protected and private day 10

--- Stage-05-(TypeScript)/dzien-10/modyfikatory-dostepow-cz2.js
+++ Stage-05-(TypeScript)/dzien-10/modyfikatory-dostepow-cz2.js
@@ -1,3 +1,5 @@
+// protected - to taki private ale pozwalający na użycie zmiennych klasy z której dziedziczymy
+// private - zmienna prywatna ale tylko w ramach klasy w której jest stworzona, bardzo często korzystamy
 class Animal3 {
     constructor(spieceName) {
         this.spieceName = spieceName;
@@ -14,7 +16,18 @@ class Human3 extends Animal3 {
         this.lifeEvents.push(eventName);
     }
     getLifeEvents() {
-        return this.lifeEvents;
+        return this.lifeEvents.filter(s => s !== 'Kompromitacja');
+        // return this.lifeEvents
+    }
+    foobar() {
+        return this.spieceName;
     }
 }
+const michal2 = new Human3('michal', 'k');
+console.log(michal2);
+michal2.addLifeEvents("Urodziny");
+michal2.addLifeEvents("Kompromitacja");
+michal2.addLifeEvents("Urodziny");
+console.log(michal2.getLifeEvents());
+console.log(michal2.foobar());
 //# sourceMappingURL=modyfikatory-dostepow-cz2.js.map
--- Stage-05-(TypeScript)/dzien-10/modyfikatory-dostepow-cz2.ts
+++ Stage-05-(TypeScript)/dzien-10/modyfikatory-dostepow-cz2.ts
@@ -1,24 +1,41 @@
+// protected - to taki private ale pozwalający na użycie zmiennych klasy z której dziedziczymy
+// private - zmienna prywatna ale tylko w ramach klasy w której jest stworzona, bardzo często korzystamy
+
+
 class Animal3 {
-    constructor(private readonly spieceName: string) {
+    constructor(protected readonly spieceName: string) {
     }
 }
 
 
 class Human3 extends Animal3 {
+
+    private lifeEvents: string[] = [];
     private isAlive: true;
-    private readonly lifeEvents: string[] = [];
 
     constructor(public readonly name: string, public readonly surname: string) {
         super('human')
     }
 
-    addLifeEvents(eventName: string) {
+    addLifeEvents(eventName: string): void {
         this.lifeEvents.push(eventName)
     }
 
-    getLifeEvents() {
-        return this.lifeEvents
+    getLifeEvents(): string[] {
+        return this.lifeEvents.filter(s => s !== 'Kompromitacja')
+        // return this.lifeEvents
+    }
+
+    foobar(): string {
+        return this.spieceName
     }
 }
 
+const michal2 = new Human3('michal', 'k')
+console.log(michal2)
+michal2.addLifeEvents("Urodziny")
+michal2.addLifeEvents("Kompromitacja")
+michal2.addLifeEvents("Urodziny")
+console.log(michal2.getLifeEvents())
+console.log(michal2.foobar())
 
