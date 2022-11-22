3cfca9f892d44f96c9c48559681a45e2c59cd1d4
sebuszqo <sekn.kn@gmail.com>
2022-11-22 10:37:09 +0100

    Uzycie typów generycznych w prostym API day 11

--- Stage-05-(TypeScript)/dzien-11/typy-generyczne.js
+++ Stage-05-(TypeScript)/dzien-11/typy-generyczne.js
@@ -7,17 +7,60 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-function getTodo(id) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const res = yield fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
-        const data = yield res.json();
-        return data;
-    });
+class TodoApi {
+    constructor() {
+        this.url = `https://jsonplaceholder.typicode.com/todos/`;
+    }
+    getSingle(id) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const res = yield fetch(`${this.url}/${id}`);
+            const data = yield res.json();
+            return data;
+        });
+    }
+    listAll() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const res = yield fetch(`${this.url}/`);
+            const data = yield res.json();
+            return data;
+        });
+    }
+    create(task) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const res = yield fetch(`${this.url}/`, {
+                method: 'POST',
+                body: JSON.stringify(task),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8'
+                },
+            });
+            const data = yield res.json();
+            return data;
+        });
+    }
 }
 (() => __awaiter(this, void 0, void 0, function* () {
-    const todo = yield getTodo(10);
-    console.log(todo.title, todo.completed ? "is completed" : "is to be done");
+    const todo = new TodoApi();
+    // console.log(await todo.getSingle(10))
+    // console.log(await todo.listAll())
+    const newToDo = yield todo.create({
+        title: "Wynieść smieci",
+        userId: 1,
+        completed: false,
+    });
+    console.log(newToDo.id);
+    console.log(yield todo.getSingle(newToDo.id));
 }))();
+// async function getTodo(id: number): Promise<SingleTodo> {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+//     const data = await res.json()
+//     return data
+// }
+//
+// (async () => {
+//     const todo = await getTodo(10)
+//     console.log(todo.title, todo.completed ? "is completed" : "is to be done")
+// })();
 // typ generyczny to tablica
 //const ar: Array<number> [1,2,3]
 //# sourceMappingURL=typy-generyczne.js.map
--- Stage-05-(TypeScript)/dzien-11/typy-generyczne.ts
+++ Stage-05-(TypeScript)/dzien-11/typy-generyczne.ts
@@ -1,20 +1,65 @@
-interface SingleTodo {
+interface SingleTodoToCreate {
     userId: number;
-    id: number;
     title: string;
     completed: boolean;
 }
 
-async function getTodo(id: number): Promise<SingleTodo> {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
-    const data = await res.json()
-    return data
+interface SingleTodo extends SingleTodoToCreate {
+    id: number;
+}
+
+
+class TodoApi {
+    private readonly url = `https://jsonplaceholder.typicode.com/todos/`
+
+    async getSingle(id: number): Promise<SingleTodo> {
+        const res = await fetch(`${this.url}/${id}`)
+        const data = await res.json()
+        return data
+    }
+
+    async listAll(): Promise<SingleTodo[]> {
+        const res = await fetch(`${this.url}/`)
+        const data = await res.json()
+        return data
+    }
+
+    async create(task: SingleTodoToCreate): Promise<SingleTodo> {
+        const res = await fetch(`${this.url}/`, {
+            method: 'POST',
+            body: JSON.stringify(task),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            },
+        });
+        const data = await res.json()
+        return data
+    }
 }
 
 (async () => {
-    const todo = await getTodo(10)
-    console.log(todo.title, todo.completed ? "is completed" : "is to be done")
+    const todo = new TodoApi()
+    // console.log(await todo.getSingle(10))
+    // console.log(await todo.listAll())
+    const newToDo = await todo.create({
+        title: "Wynieść smieci",
+        userId: 1,
+        completed: false,
+    })
+    console.log(newToDo.id)
+    console.log(await todo.getSingle(newToDo.id))
 })();
+
+// async function getTodo(id: number): Promise<SingleTodo> {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+//     const data = await res.json()
+//     return data
+// }
+//
+// (async () => {
+//     const todo = await getTodo(10)
+//     console.log(todo.title, todo.completed ? "is completed" : "is to be done")
+// })();
 
 
 // typ generyczny to tablica
