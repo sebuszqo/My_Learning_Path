a02930e1598244f6cd408777d5d384de0e7c9caa
sebuszqo <sekn.kn@gmail.com>
2023-02-03 16:17:11 +0100

    Part 10 - first usage of functional useEffect() methods

--- Stage-06-(React)/my-first-app/src/Clock.js
+++ Stage-06-(React)/my-first-app/src/Clock.js
@@ -1,21 +1,35 @@
-import {Component} from "react";
+import {Component, useEffect, useState} from "react";
 
-export class Clock extends Component {
-    state = {
-        datetime: new Date(),
-    };
 
-    componentDidMount() {
-        this.intervalId = setInterval(() => {
-            this.setState({datetime: new Date(),})
+export const Clock = props => {
+    const [DateTime, setDateTime] = useState(new Date())
+
+    useEffect(() => {
+        console.log("Only once!")
+        setInterval(() => {
+            setDateTime(new Date())
         }, 1000)
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.intervalId)
-    }
-
-    render() {
-        return <p>{this.state.datetime.toLocaleString()}</p>
-    }
+    }, [])
+    return <p>{DateTime.toLocaleString()}</p>
 }
+
+//
+// export class Clock extends Component {
+//     state = {
+//         datetime: new Date(),
+//     };
+//
+//     componentDidMount() {
+//         this.intervalId = setInterval(() => {
+//             this.setState({datetime: new Date(),})
+//         }, 1000)
+//     }
+//
+//     componentWillUnmount() {
+//         clearInterval(this.intervalId)
+//     }
+//
+//     render() {
+//         return <p>{this.state.datetime.toLocaleString()}</p>
+//     }
+// }
