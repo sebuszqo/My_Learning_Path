41fceb2e84115468861f73eed30fffce61252ecb
sebuszqo <sekn.kn@gmail.com>
2023-02-03 12:26:49 +0100

    Part8 of learning React hooks, useState, and how we cant do it

--- Stage-06-(React)/my-first-app/src/StringList.js
+++ Stage-06-(React)/my-first-app/src/StringList.js
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 export const StringList = props => {
     return <h1>{props.list.join(", ")}</h1>
 }
--- Stage-06-(React)/my-first-app/src/WinterCamp.js
+++ Stage-06-(React)/my-first-app/src/WinterCamp.js
@@ -1,13 +1,43 @@
 import {CounterOfChildren} from "./CounterOfChildren";
 import {StringList} from "./StringList";
+import {useState} from "react";
 
 export const WinterCamp = (props) => {
+    // first use-state
+    const [thingsToDo, setThingToDo] = useState("go to fridge")
+    //
+    // cannot do like that
+    // let information = 'xyz'
 
+
+    const [count, setCount] = useState(0)
+
+    // counter but It's not good idea
+    // recursion !!
+    // setTimeout(() => {
+    //     setCount((count) => count + 1)
+    // }, 1000);
+
+    setTimeout(() => {
+
+
+        setThingToDo("Zjeść ... ")
+
+        // cannot do like that
+        // information = "zyx"
+
+
+    }, 2000)
+
+    
     //.map(), .filter(), .forEach(), .reduce() - these methods are okay, we can use them to change props
     // we cannot use methods such as .pop(), .push() without copying our props
     const names = props.participants.map(person => person.name)
     return (
         <>
+            {count}
+            <br/>
+            {thingsToDo}
             <StringList list={names}/>
             <CounterOfChildren count={props.participants.length}/>
         </>
