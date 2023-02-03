a169b78cc9f2a12a1437f9d916ecf62ef119d08c
sebuszqo <sekn.kn@gmail.com>
2023-02-03 14:24:19 +0100

    Part 9.1 learning React - useState but how to do it in class components ?

--- Stage-06-(React)/my-first-app/src/WinterCamp.js
+++ Stage-06-(React)/my-first-app/src/WinterCamp.js
@@ -1,46 +1,66 @@
 import {CounterOfChildren} from "./CounterOfChildren";
 import {StringList} from "./StringList";
-import {useState} from "react";
-
-export const WinterCamp = (props) => {
-    // first use-state
-    const [thingsToDo, setThingToDo] = useState("go to fridge")
-    //
-    // cannot do like that
-    // let information = 'xyz'
+import {Component, useState} from "react";
 
 
-    const [count, setCount] = useState(0)
+// useState to know how to deal with it in class components
+export class WinterCamp extends Component {
+    state = {
+        name: "Michał",
+        lastName: "Sienkiewicz"
+    };
 
-    // counter but It's not good idea
-    // recursion !!
-    // setTimeout(() => {
-    //     setCount((count) => count + 1)
-    // }, 1000);
+    render() {
 
-    setTimeout(() => {
+        setTimeout(() => {
+            this.setState({name: "Dominik"})
 
-
-        setThingToDo("Zjeść ... ")
-
-        // cannot do like that
-        // information = "zyx"
-
-
-    }, 2000)
-
-    
-    //.map(), .filter(), .forEach(), .reduce() - these methods are okay, we can use them to change props
-    // we cannot use methods such as .pop(), .push() without copying our props
-    const names = props.participants.map(person => person.name)
-    return (
-        <>
-            {count}
-            <br/>
-            {thingsToDo}
-            <StringList list={names}/>
-            <CounterOfChildren count={props.participants.length}/>
-        </>
-
-    )
+        }, 2000)
+        return <h1>{this.state.name}</h1>
+    }
 }
+
+
+//
+// export const WinterCamp = (props) => {
+//     // first use-state
+//     const [thingsToDo, setThingToDo] = useState("go to fridge")
+//     //
+//     // cannot do like that
+//     // let information = 'xyz'
+//
+//
+//     const [count, setCount] = useState(0)
+//
+//     // counter, but It's not good idea
+//     // recursion !!
+//     // setTimeout(() => {
+//     //     setCount((count) => count + 1)
+//     // }, 1000);
+//
+//     setTimeout(() => {
+//
+//
+//         setThingToDo("Zjeść ... ")
+//
+//         // cannot do like that
+//         // information = "zyx"
+//
+//
+//     }, 2000)
+//
+//
+//     //.map(), .filter(), .forEach(), .reduce() - these methods are okay, we can use them to change props
+//     // we cannot use methods such as .pop(), .push() without copying our props
+//     const names = props.participants.map(person => person.name)
+//     return (
+//         <>
+//             {count}
+//             <br/>
+//             {thingsToDo}
+//             <StringList list={names}/>
+//             <CounterOfChildren count={props.participants.length}/>
+//         </>
+//
+//     )
+// }
