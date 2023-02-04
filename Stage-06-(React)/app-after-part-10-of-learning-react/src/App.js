03615791c3a594eba03f6bc7e3cb58df1bdeb6be
sebuszqo <sekn.kn@gmail.com>
2023-02-04 22:07:41 +0100

    Part 11.1 - learning React - first form onClick onSubmit

--- Stage-06-(React)/app-after-part-10-of-learning-react/src/App.js
+++ Stage-06-(React)/app-after-part-10-of-learning-react/src/App.js
@@ -1,24 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
+import {Component, useState} from "react";
 import {Clock} from "./Clock";
 import {CrazyInput} from "./CrazyInput";
 import {ProgressBar} from "./ProgressBar";
 import {AnimatedProgressBar} from "./AnimatedProgressBar";
+//
+// export class App extends Component {
+//     state = {
+//         isSend: false
+//     }
+//
+//     sendForm = (event) => {
+//         event.preventDefault()
+//         this.setState({
+//             isSend: true
+//         })
+//     }
+//
+//     render() {
+//         return <form action="" onSubmit={this.sendForm}>
+//             <p>{this.state.isSend ? "I sent your data" : "Give me your data"}</p>
+//             <input type="text"/>
+//             <button>Wyślij</button>
+//         </form>
+//     }
+// }
 
+//
 export const App = props => {
-    return (
-        <>
-            {/*<div style={{backgroundColor: "black"}}>*/}
-            {/*    <CrazyInput/>*/}
-            {/*</div>*/}
-            {/*<CrazyInput/>*/}
-            {/*<CrazyInput isPwd/>*/}
-            {/*<ProgressBar percentage={100}/>*/}
-            {/*<ProgressBar percentage={75}/>*/}
-            {/*<ProgressBar percentage={50}/>*/}
-            {/*<ProgressBar percentage={25}/>*/}
-            <AnimatedProgressBar/>
-        </>
-    )
+    function clicked(event) {
+        event.preventDefault();
+    }
+
+    const sendForm = () => {
+
+    }
+
+    return <form action="" onSubmit={sendForm}>
+        <input type="text"/>
+        <button>Wyślij</button>
+    </form>
+    return <button onClick={clicked}>Click me!</button>;
+// return (
+//     <>
+//         {/*<div style={{backgroundColor: "black"}}>*/}
+//         {/*    <CrazyInput/>*/}
+//         {/*</div>*/}
+//         {/*<CrazyInput/>*/}
+//         {/*<CrazyInput isPwd/>*/}
+//         {/*<ProgressBar percentage={100}/>*/}
+//         {/*<ProgressBar percentage={75}/>*/}
+//         {/*<ProgressBar percentage={50}/>*/}
+//         {/*<ProgressBar percentage={25}/>*/}
+//         <AnimatedProgressBar/>
+//     </>
+// )
 }
