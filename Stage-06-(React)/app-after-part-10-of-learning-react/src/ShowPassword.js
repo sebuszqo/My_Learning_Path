e242af84b758f76f773ccb54e6281435a197756e
sebuszqo <sekn.kn@gmail.com>
2023-02-05 15:46:57 +0100

    Part13 of learning React - password input - show and do not show system :)

--- Stage-06-(React)/app-after-part-10-of-learning-react/src/ShowPassword.js
+++ Stage-06-(React)/app-after-part-10-of-learning-react/src/ShowPassword.js
@@ -2,5 +2,9 @@ import {useState} from "react";
 
 export const ShowPassword = (props) => {
     const [visible, setVisible] = useState(false)
-    return <input type="password"/>
+    return <>
+        <input type={visible ? "text" : "password"}/>
+        {/*<button onClick={() => setVisible((prevVisible) => prevVisible ? false : true)}>{visible ? "🔒" : "👁"}️</button>*/}
+        <button onMouseDown={() => setVisible(true)} onMouseUp={() => setVisible(false)}>{visible ? "🔒" : "👁"}️</button>
+    </>
 }
