4ae3c7d86011bca9d78a4ade2fa2c0021cf26fa8
sebuszqo <sekn.kn@gmail.com>
2023-02-06 19:21:59 +0100

    Part 15 the end of little task of user list :)

--- Stage-06-(React)/list-of-people/src/App.js
+++ Stage-06-(React)/list-of-people/src/App.js
@@ -6,9 +6,10 @@ import {useState} from "react";
 
 export function App(props) {
     const [sorted, setSorted] = useState(false);
+    // .sort --> it's mutable method, so I need to use array dispersion
     const list = [...props.list]
         .sort((a, b) => sorted ? a.first_name.localeCompare(b.first_name) : b.first_name.localeCompare(a.first_name))
-        .map(person => <SingleItem item={person}/>)
+        .map(person => <SingleItem item={person} key={person.username}/>)
     return (
         <div className={"divList"}>
             <div>
--- Stage-06-(React)/list-of-people/src/SingleItem.js
+++ Stage-06-(React)/list-of-people/src/SingleItem.js
@@ -5,7 +5,7 @@ export const SingleItem = (props) => {
     const bigLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
     return <li key={props.item.username}>
         <img src={`https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/${props.item.picture}`}
-             alt="avatar"/>
+             alt={`Avatar of ${props.item.first_name}`}/>
         <div>
             <p>First Name: <strong>{bigLetter(props.item.first_name)}</strong></p>
             <p>Last Name: <strong>{bigLetter(props.item.last_name)}</strong></p>
