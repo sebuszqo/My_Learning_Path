b132ac27d64552588e57d70c2f83272d59b9573d
sebuszqo <sekn.kn@gmail.com>
2023-02-07 14:33:14 +0100

    Added simple input to deal with colors to train inputs in React part 19 :)

--- Stage-06-(React)/app-after-part-10-of-learning-react/src/ components/select/SelectRGBDiv.js
+++ Stage-06-(React)/app-after-part-10-of-learning-react/src/ components/select/SelectRGBDiv.js
@@ -2,6 +2,11 @@ import {useState} from "react";
 
 export const SelectRGBDiv = props => {
     const [color, setColor] = useState("white");
+
+    const sendForm = (event) => {
+        event.preventDefault()
+        console.log(event.target.value)
+    }
     return (
         <>
             <div style={
@@ -11,13 +16,14 @@ export const SelectRGBDiv = props => {
                     backgroundColor: `${color}`,
                 }}
             ></div>
-            <form>
+            <form onSubmit={sendForm}>
                 <select name="color" value={color} onChange={e => setColor(e.target.value)}>
                     <option value="">-Wybierz-</option>
                     <option value="red">red</option>
                     <option value="green">green</option>
                     <option value="blue">blue</option>
                 </select>
+                <input type="text" value={color} onChange={e => setColor(e.target.value)}/>
             </form>
         </>
     )
