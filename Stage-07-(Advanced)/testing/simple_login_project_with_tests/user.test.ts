84e4eec969de9bebe5f72554dfb81278a6ea96cc
sebuszqo <sekn.kn@gmail.com>
2023-02-18 19:04:34 +0100

    TDD start of first little projects

---
+++ Stage-07-(Advanced)/testing/simple_login_project_with_tests/user.test.ts
@@ -0,0 +1,21 @@
+// TDD for user
+let user: User;
+
+beforeAll(async () => {
+    user = new User();
+})
+
+test("User should not be logged in at the beginning", () => {
+    expect(user.loggedIn).toEqual(false);
+});
+
+test("User should have no e-mail at the beginning", () => {
+    expect(user.emial).toBeNull();
+});
+
+test("User logged in state should not be modified outside of class", () => {
+    expect(() => {
+        user.loggedIn = true
+    }).toThrow();
+});
+
