f67f90293df4f2cd6a6ced3e09c1df0b14fb0c53
sebuszqo <sekn.kn@gmail.com>
2023-02-19 14:20:24 +0100

    prettier and tests config

--- Stage-07-(Advanced)/testing/simple_login_project_with_tests/user.test.ts
+++ Stage-07-(Advanced)/testing/simple_login_project_with_tests/user.test.ts
@@ -2,20 +2,19 @@
 let user: User;
 
 beforeAll(async () => {
-    user = new User();
-})
+  user = new User();
+});
 
 test("User should not be logged in at the beginning", () => {
-    expect(user.loggedIn).toEqual(false);
+  expect(user.loggedIn).toEqual(false);
 });
 
 test("User should have no e-mail at the beginning", () => {
-    expect(user.emial).toBeNull();
+  expect(user.emial).toBeNull();
 });
 
 test("User logged in state should not be modified outside of class", () => {
-    expect(() => {
-        user.loggedIn = true
-    }).toThrow();
+  expect(() => {
+    user.loggedIn = true;
+  }).toThrow();
 });
-
