929fb74ef6c5b05bcbd2b7022e5317ae90305ca3
sebuszqo <sekn.kn@gmail.com>
2023-02-22 19:27:26 +0100

    Adding commands to config ecosystem.config.js pm2

--- Stage-07-(Advanced)/testing/simple_login_project_with_tests/ecosystem.config.js
+++ Stage-07-(Advanced)/testing/simple_login_project_with_tests/ecosystem.config.js
@@ -2,9 +2,20 @@ module.exports = {
   apps: [
     {
       name: "myapp",
-      script: "dist/index.js",
+      script: "./dist/index.js",
       autorestart: true,
       max_memory_restart: "2G",
+      exec_mode: "cluster",
+      instances: -1,
     },
   ],
 };
+
+// we can use   number_we_want_to_use/"max"/-1
+// max  - maximum number of threads
+// -1 - maximum - 1 number of threads -- the best!
+// npm install pm2@latest -g
+
+// pm2 start ecosystem.config.js - in directory
+
+// pm2 start ./dist/index.js
